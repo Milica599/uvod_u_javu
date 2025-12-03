cdd3d1a9549738cb389e51443e685007af18db5c
Milica599 <mirkovicmilica@03gmail.com>
2025-12-03 20:11:21 +0100

    Java core 9 cas uvod u oop

---
+++ nine/oop/Cat.java
@@ -0,0 +1,15 @@
+package com.itacademija.nine.oop;
+
+/**
+ * OOP : abstraction,
+ */
+public class Cat extends Animal{
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void playSound() {
+
+    }
+}
